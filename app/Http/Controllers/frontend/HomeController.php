0b6e2ff95fe8a2e321b99cd712b9175ff69ac7f0
AllsVeam <alaansvega@gmail.com>
2025-11-26 15:25:23 -0600

    Fix: route de index home

--- app/Http/Controllers/frontend/HomeController.php
+++ app/Http/Controllers/frontend/HomeController.php
@@ -24,7 +24,8 @@ class HomeController extends Controller
     }
 
     public function index(){
-        return view('pymais.frontend.home');
+        return view('frontend.builder-home.index');
+        //return view('pymais.frontend.home'); -> Anterior dashboard
     }
 
     public function applicant_signup(){
