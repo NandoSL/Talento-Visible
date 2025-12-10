428e9eb1d201580ebd2faf6e12ab5a889c432a0a
Yessica Viridiana Leon Mendiola <LeonMendiolaYV>
2025-12-10 12:58:23 -0600

    Fix:Image

--- resources/views/instructor/no_data.blade.php
+++ resources/views/instructor/no_data.blade.php
@@ -1,9 +1,7 @@
 <div class="card-centered-section">
     <div class="card-middle-banner">
-        <div class="card-centered-section d-flex flex-column justify-content-center align-items-center" style="min-height: 400px; padding: 20px;">
-        <div class="icono-vacio-bootcamp mb-3">
-                <i class="fas fa-search"></i>
-         {{-- <img src="{{ get_image('assets/backend/images/img/no-file-search.jpeg') }}" alt="">--}}
+       
+         <img src="{{ get_image('assets/backend/images/img/no-file.jpeg') }}" alt="">
     </div>
     <p class="title2 fs-20px text-center ">{{ get_phrase('No data found') }}</p>
 </div>
